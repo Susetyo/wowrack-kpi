0f98d5579db58442c154ef457b2c87d7941fbe61
Susetyo Pambudi <susetyamulia@gmail.com>
2022-08-13 20:13:01 +0700

    feat: add antd lib

--- src/commons/components/Testing.tsx
+++ src/commons/components/Testing.tsx
@@ -1,5 +1,11 @@
+import {Button} from 'antd';
 const Testing = () => {
-  return(<h1>This is Testing</h1>)
+  return(<>
+    <h1>This is Testing</h1>
+    <Button type="primary" size="large">
+        Primary
+      </Button>
+  </>)
 }
 
 export default Testing
